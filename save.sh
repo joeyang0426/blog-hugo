0dee31c2aa64093ee2124f5bda7ce86870c7e0ef
joe.yang <true8877@gmail.com>
2019-09-16 01:05:25 +0800

    rebuilding site Mon Sep 16 01:05:24     2019

---
+++ save.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
